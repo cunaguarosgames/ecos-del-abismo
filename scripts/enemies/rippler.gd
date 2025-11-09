4bef3461d7f624d7944487f8dd0d6e3d9cac512c
Alejandra <alejcast18@gmail.com>
2025-11-08 22:17:49 -0400

    add health_bar

--- scripts/enemies/rippler.gd
+++ scripts/enemies/rippler.gd
@@ -4,8 +4,14 @@ var speed = 25
 var player_chase = false
 var player = null
 var health = 30
-var current_health = 100.0
-var max_health = 100.0
+var current_health = 20.0
+var max_health = 20.0
+@onready var progress_bar: ProgressBar = $ProgressBar
+
+
+func _ready():
+	progress_bar.max_value = max_health
+	progress_bar.value = max_health
 
 func _physics_process(delta: float) -> void:
 	if player_chase and is_instance_valid(player):
@@ -24,16 +30,17 @@ func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false
 	
-func get_damage(damage:int):
-	health-=damage
-	print(health)
-	
 func take_damage(amount : float):
 	current_health -= amount
+	
+	
+	update_health()
 	
 	if current_health <= 0:
 		print("el personaje no tiene salud")
 func update_health():
-	pass
-		
-	
+	if progress_bar: 
+		progress_bar.value = current_health
+		if progress_bar.value < max_health: 
+			progress_bar.show()
+			
