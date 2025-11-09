bb202a62598c0a306107a469fb274220eb96c57d
RaulDevGames <rauldevgames@gmail.com>
2025-11-08 21:28:24 -0400

    add damage to BasicAttack

--- scripts/attacks/basic_attack.gd
+++ scripts/attacks/basic_attack.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @export var speed: float = 200.0
 @export var lifetime: float = 2.0
+@export var damage: int = 5
+
 var direction: Vector2 = Vector2.RIGHT
 
 func _ready() -> void:
@@ -13,4 +15,6 @@ func _physics_process(delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("map") or body.is_in_group("enemies"):
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
 		queue_free()
