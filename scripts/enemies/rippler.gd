c1f8f3a224c8e9eab14708b2b308d899d39e5f4b
Alejandra <alejcast18@gmail.com>
2025-11-08 21:22:48 -0400

    add progress_bar

--- scripts/enemies/rippler.gd
+++ scripts/enemies/rippler.gd
@@ -4,7 +4,8 @@ var speed = 25
 var player_chase = false
 var player = null
 var health = 30
-
+var current_health = 100.0
+var max_health = 100.0
 
 func _physics_process(delta: float) -> void:
 	if player_chase and is_instance_valid(player):
@@ -26,3 +27,13 @@ func _on_detection_area_body_exited(body: Node2D) -> void:
 func get_damage(damage:int):
 	health-=damage
 	print(health)
+	
+func take_damage(amount : float):
+	current_health -= amount
+	
+	if current_health <= 0:
+		print("el personaje no tiene salud")
+func update_health():
+	pass
+		
+	
