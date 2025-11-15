098fcd53d9c052b039bdf51665a68ddc3b7af397
Alejandra <alejcast18@gmail.com>
2025-11-14 22:25:48 -0400

    add new changes in stalker

--- scenes/enemies/stalker/attack.gd
+++ scenes/enemies/stalker/attack.gd
@@ -13,19 +13,21 @@ func start() -> void:
 	stalker.play_main_animation("attack")
 
 func _on_attack_cooldown_timer_timeout() -> void:
-	var direction_vector = (stalker.player.position - stalker.position)
-	var distance = direction_vector.length()
-	
-	var attack_range = 60
-	
-	if distance <= attack_range:
-		state_machine.change_to("Attack")
-		return
-	if not stalker.player:
-		state_machine.change_to("Idle") 
-	else:
-		state_machine.change_to("Chasing") 
+	if stalker.player:
+		var direction_vector = (stalker.player.position - stalker.position)
+		var distance = direction_vector.length()
+		var attack_range = 60
+		
+		if distance <= attack_range:
+			state_machine.change_to("Attack")
+			return
+			state_machine.change_to("Chasing")
+			state_machine.change_to("Idle")
 
 func end():
 	stalker.can_attack = true
 	stalker.attack_cooldown_timer.stop()
+	
+func on_physics_process(_delta: float) -> void:
+	if stalker.current_health <=0: 
+		state_machine.change_to("Death")
