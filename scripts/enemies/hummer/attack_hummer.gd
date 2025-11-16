25b7d267d8acd48528faf6767fea83fdc988b479
Deivid vargas <deividjvargas30@gmail.com>
2025-11-16 15:37:56 -0400

    finish hummer enemie

--- scripts/enemies/hummer/attack_hummer.gd
+++ scripts/enemies/hummer/attack_hummer.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @export var speed := 200
 @onready var life_time = $Timer
+@onready var animeteSprite = $AnimatedSprite2D
 var target: Node2D = null
 var damage := 0
 var knockback_force := 0
@@ -13,6 +14,7 @@ func _on_timer_timeout() -> void:
 func _ready() -> void:
 	life_time.timeout.connect(_on_timer_timeout)
 	life_time.start()
+	animeteSprite.play("default")
 
 func _process(delta):
 	if !target or !is_instance_valid(target):
@@ -20,7 +22,12 @@ func _process(delta):
 		return
 
 	var direction = (target.global_position - global_position).normalized()
+
+	# Rotar hacia el objetivo
+	rotation = direction.angle()
+
 	global_position += direction * speed * delta
+	
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
