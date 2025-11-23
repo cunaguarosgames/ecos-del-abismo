7c84558e234b58dbc3d84fc784e27c5a4e6f5831
RaulDevGames <rauldevgames@gmail.com>
2025-11-23 13:45:03 -0400

    add player die and fix hit states

--- scripts/player/states/hit.gd
+++ scripts/player/states/hit.gd
@@ -1,21 +1,16 @@
 extends PlayerStateBase
 
-@export var knockback_decay := 1000.0
-var velocity: Vector2 = Vector2.ZERO
+@export var knockback_decay := 800.0
 
 func start():
 	player.can_attack = false
 	player.animated_sprite_2d.play("hurt")
 
-func setup_knockback(initial_velocity: Vector2):
-	velocity = initial_velocity
-
 func on_physics_process(delta: float) -> void:
-	player.velocity = velocity
+	player.velocity = player.velocity.move_toward(Vector2.ZERO, knockback_decay * delta)
 	player.move_and_slide()
-
-	velocity = velocity.move_toward(Vector2.ZERO, knockback_decay * delta)
-	if velocity.length() < 10.0:
+	
+	if player.velocity.length() < 10.0:
 		state_machine.change_to(player.states.Idle)
 
 func end():
