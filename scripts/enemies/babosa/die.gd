1e152ef25b370f73f20767f58fa5db75943e508d
Deivid vargas <deividjvargas30@gmail.com>
2025-11-29 02:56:07 -0400

    add  probability drop the health item on hummer and babosa

--- scripts/enemies/babosa/die.gd
+++ scripts/enemies/babosa/die.gd
@@ -1,12 +1,18 @@
 extends babosaStateBase
 
+
 func start() -> void:
 	babosa.velocity = Vector2.ZERO
 	babosa.animSprite.play("explotion")
 	babosa.animSprite.animation_finished.connect(_on_animation_finished, CONNECT_ONE_SHOT)
 
 func _on_animation_finished() -> void:
+	var random_chance = randi_range(1, 100)
 	explotion()       
+	if random_chance <= 35:
+		var health = preload("res://scenes/items/health_item.tscn").instantiate()
+		health.global_position = babosa.global_position
+		get_parent().get_parent().get_parent().add_child(health)
 	babosa.queue_free()
 
 func explotion() -> void: 
--- scripts/enemies/hummer/dead.gd
+++ scripts/enemies/hummer/dead.gd
@@ -1,8 +1,16 @@
 extends hummerStateBase
 
+func _on_death(): 
+	var random_chance = randi_range(1, 100)
+	if random_chance <= 20:
+		var health = preload("res://scenes/items/health_item.tscn").instantiate()
+		health.global_position = hummer.global_position
+		get_parent().get_parent().get_parent().add_child(health)
+	hummer.queue_free()
+	
 func start() -> void:
 	hummer.velocity = Vector2.ZERO
 	hummer.animSprite.play("die")
 
 	if not hummer.animSprite.animation_finished.is_connected(hummer.queue_free):
-		hummer.animSprite.animation_finished.connect(hummer.queue_free, CONNECT_ONE_SHOT)
+		hummer.animSprite.animation_finished.connect(_on_death, CONNECT_ONE_SHOT)
