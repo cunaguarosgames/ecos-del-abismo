998001e100e4c6707dd56fbbda3b7b497174683e
RaulDevGames <rauldevgames@gmail.com>
2025-12-01 01:40:18 -0400

    add activate music when boss battle start

--- scenes/rooms/rocks.gd
+++ scenes/rooms/rocks.gd
@@ -1,8 +1,18 @@
 extends StaticBody2D
 
+@export var bpm: float = 0.0
+
 @onready var rocks: Sprite2D = $Rocks
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var audio_stream_player: AudioStreamPlayer = $"../AudioStreamPlayer"
+@onready var boss_music: AudioStreamPlayer = $"../BossMusic"
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	rocks.visible = true
 	collision_shape_2d.set_deferred("disabled", false)
+	
+	RhythmManager.end_music(audio_stream_player)
+	
+	RhythmManager.start_music(boss_music)
+	if not bpm == 0.0:
+		RhythmManager.set_bpm(bpm)
