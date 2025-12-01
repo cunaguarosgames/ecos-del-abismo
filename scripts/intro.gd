28d78cd355223fb289bf8e8bd3487a6d21be5164
RaulDevGames <rauldevgames@gmail.com>
2025-12-01 16:02:05 -0400

    changes in main menu

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -9,18 +9,13 @@ func _ready() -> void:
 func fade_in_logo() -> void:
 	var t := create_tween()
 	
-	# Fade in de 1.5 segundos
 	t.tween_property(black_rect, "modulate:a", 0.0, 1.5)
 	
-	# Esperar 3 segundos con el logo visible
 	t.tween_interval(3.0)
 	
-	# Opcional: fade out a negro para terminar la intro
 	t.tween_property(black_rect, "modulate:a", 1.0, 1.5)
 	
-	# Después de eso cargas tu siguiente escena
 	t.tween_callback(Callable(self, "_go_to_next_scene"))
 
 func _go_to_next_scene():
-	# Aquí cambias de escena
 	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
